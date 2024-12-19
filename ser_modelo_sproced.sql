34d011727ee0cb631e508ebd2f247946443e9cf3
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-12-19 15:07:46 -0300

    ok_modelo_sproced
    Finalizada a criação de stored procedure modelo

--- ser_modelo_sproced.sql
+++ ser_modelo_sproced.sql
@@ -4,5 +4,24 @@ Exercicio do livro sql server
 Aluno: Guglielmo Targino.
 Data: 19dez24
 Versão: v0
+pg 557
 
 */
+
+create procedure nome_proced [ ;numero]
+[{@param1 tipo_dado}
+[varying][ =padrao][output]
+][,...n]
+
+[with
+{recompile|encryption|recompile,encryption}]
+[for replication]
+as
+begin transaction
+	declare @xy int
+	set @xy=2
+	if (@xy>2)
+	print 'numero maior que permitido'
+	raiserror('procure adm %d',16,1,@xy)
+	return()
+end
