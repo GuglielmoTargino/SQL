66efe820973671e4537f2c3cea625f7012415765
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-01-24 13:53:30 -0300

    fim_inner_jooin
    Finalizado o exercício com inner join.

--- ssm_inner_join.sql
+++ ssm_inner_join.sql
@@ -1,10 +1,14 @@
 
 use connec_test
 
+select * from ti
 
+delete from ti where nome='j'
+insert into ti(id,nome) values(81,'j')
+update usuario set cargo='ger' where cargo='gt'
 
-select count(ti.id),ti.id
+select ti.id as id_ti, usuario.senha as senha_usuario, usuario.nome_usu as nome_usuario
 from ti
 inner join usuario
 
-on ti.id<(ti.id*2)
+on (ti.id*2)>usuario.senha
