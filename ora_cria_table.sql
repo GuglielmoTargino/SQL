3982910fc79e5590e34fa3672e58b1211b2bd0f5
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-01-27 19:58:36 -0300

    audio_ric
    Audio da ric 20250125

---
+++ ora_cria_table.sql
@@ -0,0 +1,8 @@
+ALTER TABLE PETS ADD id_pet int
+
+insert into pets(nome,id_pet) values('tod',8)
+
+CREATE TABLE usuario
+  (nome_usu char(10), 
+  id_usu int
+  )
--- ssm_inner_join.sql
+++ ssm_inner_join.sql
@@ -7,6 +7,9 @@ delete from ti where nome='j'
 insert into ti(id,nome) values(81,'j')
 update usuario set cargo='ger' where cargo='gt'
 
+
+
+
 select ti.id as id_ti, usuario.senha as senha_usuario, usuario.nome_usu as nome_usuario
 from ti
 inner join usuario
